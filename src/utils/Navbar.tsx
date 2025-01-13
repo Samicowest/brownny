f922aca7068cf6c2767418057a78b84bdda3ee91
Samicowest <samicowest@gmail.com>
2025-01-13 12:45:37 +0100

    working still going on

--- src/utils/Navbar.tsx
+++ src/utils/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { IoPersonOutline } from "react-icons/io5";
 import { AiOutlineShopping } from "react-icons/ai";
 import { FaBars, FaTimes } from "react-icons/fa";
